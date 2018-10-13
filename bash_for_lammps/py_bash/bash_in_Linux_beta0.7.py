de079fef26f28fa67dfe5b633ab1d3ed5336c174
David_Lzy <li19931020li@gmail.com>
2018-10-13 12:59:38 +0800

    photo added

--- bash_for_lammps/py_bash/bash_in_Linux_beta0.7.py
+++ bash_for_lammps/py_bash/bash_in_Linux_beta0.7.py
@@ -12,83 +12,86 @@ import subprocess
 disk_path = os.path.dirname(os.path.dirname(__file__))
 Data_location = disk_path + '/Data/ready_to_run/'
 in_file_name = 'location.2dplasma'
-Origin_in_file_path = disk_path + '/infile/' +in_file_name
+Origin_in_file_path = disk_path + '/infile/' + in_file_name
 Linux_Bash = 'mpirun -np 19 /home/dpmd0/lammps/src/lmp_ubuntu <' + in_file_name
 
-
 compare = []
-compare_number = [i for i in range(0,1)]#从0到9左闭右开
+compare_number = [i for i in range(0, 1)]  #从0到9左闭右开
 compare_str = 'Compare'
 
 beta = []
-beta_number = [0.1*i for i in range(0,11)]
+beta_number = [0.1 * i for i in range(0, 11)]
 #beta_number = [0.01,0.02,0.04,0.08,0.12,0.16,0.3,0.6,0.8,0.9]#[0.1*i for i in range(2,3)]
 #beta_number = [0.03,0.4,0.05,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.85,0.95]#[0.1*i for i in range(2,3)]
 #beta_number = [0.02,0.8,0.95]
 beta_str = 'Beta'
 
 kappa = []
-kappa_number = [2,]
+kappa_number = [
+    2,
+]
 kappa_str = 'Kappa'
 
 gamma = []
-gamma_number = ['20']#,'300']
+gamma_number = ['20']  #,'300']
 gamma_str = 'Gamma'
 
 particle_N = []
-particle_N_number = ['16384']#'1024','4096',
+particle_N_number = ['16384']  #'1024','4096',
 particle_N_str = 'N'
 
-str_paramter = [kappa_str,gamma_str,beta_str,particle_N_str,compare_str]
+str_paramter = [kappa_str, gamma_str, beta_str, particle_N_str, compare_str]
 
 for i in compare_number:
     temp = compare_str + str(i)
-    compare = numpy.append(compare,temp)
+    compare = numpy.append(compare, temp)
 
 for i in gamma_number:
     temp = gamma_str + str(i)
-    gamma = numpy.append(gamma,temp)
+    gamma = numpy.append(gamma, temp)
 
 for i in beta_number:
     temp = beta_str + str(i)
-    beta = numpy.append(beta,temp)
+    beta = numpy.append(beta, temp)
 
 for i in particle_N_number:
     temp = particle_N_str + str(i)
-    particle_N = numpy.append(particle_N,temp)
+    particle_N = numpy.append(particle_N, temp)
 
 for i in kappa_number:
     temp = kappa_str + str(i)
-    kappa = numpy.append(kappa,temp)
+    kappa = numpy.append(kappa, temp)
 
-All_parameter = [kappa,gamma,beta,particle_N,compare]
+All_parameter = [kappa, gamma, beta, particle_N, compare]
 
 All_path = ['']
 win_path = ['']
 for i in range(len(All_parameter)):
     path_number = len(All_path)
     parameter_number = len(All_parameter[i])
-    temp1 = [['' for  a in range(path_number)] for b in  range(parameter_number)]
-    temp2 = [['' for  a in range(path_number)] for b in  range(parameter_number)]
-    for j in range(parameter_number):    
-        for k in range(path_number):            
-            temp1[j][k] = All_path[k]+All_parameter[i][j]+ '/' 
-            temp2[j][k] = win_path[k]+All_parameter[i][j]+ '\\'           
+    temp1 = [['' for a in range(path_number)] for b in range(parameter_number)]
+    temp2 = [['' for a in range(path_number)] for b in range(parameter_number)]
+    for j in range(parameter_number):
+        for k in range(path_number):
+            temp1[j][k] = All_path[k] + All_parameter[i][j] + '/'
+            temp2[j][k] = win_path[k] + All_parameter[i][j] + '\\'
     All_path = []
-    win_path = [] 
-    All_path = numpy.append(All_path,temp1)  
-    win_path = numpy.append(win_path,temp2)
+    win_path = []
+    All_path = numpy.append(All_path, temp1)
+    win_path = numpy.append(win_path, temp2)
 
-def modify_file(tfile,old,new):
+
+def modify_file(tfile, old, new):
     try:
-        lines=open(tfile,'r').readlines()
-        flen=len(lines)-1
+        lines = open(tfile, 'r').readlines()
+        flen = len(lines) - 1
         for i in range(flen):
             if old in lines[i]:
-                lines[i]=lines[i].replace(old,new)
-        open(tfile,'w').writelines(lines)       
+                lines[i] = lines[i].replace(old, new)
+        open(tfile, 'w').writelines(lines)
     except Exception as e:
         print(e)
+
 
 for i in range(len(All_path)):
 
@@ -98,21 +101,22 @@ for i in range(len(All_path)):
     except Exception as e:
         print(e)
 
-    shutil.copy(Origin_in_file_path,in_file_location)
+    shutil.copy(Origin_in_file_path, in_file_location)
 
     number_begin = [0] * len(str_paramter)
-    number_end   = [0 for a in  range(len(str_paramter))]
-    new_str      = '' 
+    number_end = [0 for a in range(len(str_paramter))]
+    new_str = ''
 
     for j in range(len(str_paramter)):
-        number_begin[j] = All_path[i].find(str_paramter[j]) + len(str_paramter[j])          
-    for j in range(len(str_paramter)-1):            
-        number_end[j]   = All_path[i].find(str_paramter[j+1]) - 1       
-    number_end[len(str_paramter)-1] = All_path[i][len(All_path[i])-1]
-    
+        number_begin[j] = All_path[i].find(str_paramter[j]) + len(
+            str_paramter[j])
+    for j in range(len(str_paramter) - 1):
+        number_end[j] = All_path[i].find(str_paramter[j + 1]) - 1
+    number_end[len(str_paramter) - 1] = All_path[i][len(All_path[i]) - 1]
+
     for j in range(len(str_paramter)):
         #print(str_paramter[j])
-        if  str_paramter[j] == 'N':
+        if str_paramter[j] == 'N':
 
             old_str = 'region          box     block'
             if All_path[i][number_begin[j]:number_end[j]] == '1024':
@@ -121,71 +125,78 @@ for i in range(len(All_path)):
                 new_str = old_str + '           -32 32 -16 16 -0.3 0.3'
             elif All_path[i][number_begin[j]:number_end[j]] == '16384':
                 new_str = old_str + '           -64 64 -32 32 -0.3 0.3'
-            modify_file(in_file_location+in_file_name,old_str,new_str)
+            modify_file(in_file_location + in_file_name, old_str, new_str)
 
-        if  str_paramter[j] == 'Compare':
+        if str_paramter[j] == 'Compare':
 
             old_str = 'variable        random1'
-            new_str = old_str + '\t\t\tequal' + '\t\t' +  str(random.randint(0,10000))
-            modify_file(in_file_location+in_file_name,old_str,new_str)
+            new_str = old_str + '\t\t\tequal' + '\t\t' + str(
+                random.randint(0, 10000))
+            modify_file(in_file_location + in_file_name, old_str, new_str)
 
             old_str = 'variable        random2'
-            new_str = old_str + '\t\t\tequal' + '\t\t' +  str(random.randint(0,10000))
-            modify_file(in_file_location+in_file_name,old_str,new_str)
+            new_str = old_str + '\t\t\tequal' + '\t\t' + str(
+                random.randint(0, 10000))
+            modify_file(in_file_location + in_file_name, old_str, new_str)
 
             old_str = 'variable        random3'
-            new_str = old_str + '\t\t\tequal' + '\t\t' +  str(random.randint(0,10000))
-            modify_file(in_file_location+in_file_name,old_str,new_str)
+            new_str = old_str + '\t\t\tequal' + '\t\t' + str(
+                random.randint(0, 10000))
+            modify_file(in_file_location + in_file_name, old_str, new_str)
 
         else:
 
-            old_str = 'variable        ' + str_paramter[j] 
-            
-            new_str = old_str + '\t\t\tequal' + '\t\t' + All_path[i][number_begin[j]:number_end[j]]
-            #print(new_str)
-            modify_file(in_file_location+in_file_name,old_str,new_str) 
+            old_str = 'variable        ' + str_paramter[j]
 
+            new_str = old_str + '\t\t\tequal' + '\t\t' + All_path[i][number_begin[j]:
+                                                                     number_end[j]]
+            #print(new_str)
+            modify_file(in_file_location + in_file_name, old_str, new_str)
 
 log_location = disk_path + '/log.txt'
 f = open(log_location, 'a+')
 f.writelines('=========================================================\n')
 f.writelines('this MD simulation start at ')
-f.writelines( time.strftime("%Y-%m-%d %H:%M:%S \n", time.localtime()) )
+f.writelines(time.strftime("%Y-%m-%d %H:%M:%S \n", time.localtime()))
 f.writelines('All paramters list here\n')
 for i in All_parameter:
-    j = str(i)+'\n'
+    j = str(i) + '\n'
     f.writelines(j)
 f.writelines('ALl Windows path ready to run list here\n')
 f.writelines('*****************************************\n')
 for i in win_path:
-    j = str(i)+'\n'
+    j = str(i) + '\n'
     f.writelines(j)
 f.writelines('******************************************\n')
 f.close()
 
-
 f = open(log_location, 'a+')
 for i in range(len(All_path)):
-    in_file_location = Data_location + All_path[i] 
+    in_file_location = Data_location + All_path[i]
     try:
-        subprocess.check_call(Linux_Bash,shell=True,cwd=in_file_location)
-        os.rename(in_file_location+in_file_name,in_file_location+'py.'+in_file_name)
-        if in_file_name == 'flux.2dplasma' :
-            os.remove(in_file_location+'2dp_EACF_1.lammpstrj')
-            os.remove(in_file_location+'2dp_EACF_2.lammpstrj')
-            os.remove(in_file_location+'2dp_EACF_3.lammpstrj')
-            os.remove(in_file_location+'2dp_EACF_4.lammpstrj')
-            os.remove(in_file_location+'2dp_EACF_5.lammpstrj')
+        subprocess.check_call(Linux_Bash, shell=True, cwd=in_file_location)
+        os.rename(in_file_location + in_file_name,
+                  in_file_location + 'py.' + in_file_name)
+        if in_file_name == 'flux.2dplasma':
+            os.remove(in_file_location + '2dp_EACF_1.lammpstrj')
+            os.remove(in_file_location + '2dp_EACF_2.lammpstrj')
+            os.remove(in_file_location + '2dp_EACF_3.lammpstrj')
+            os.remove(in_file_location + '2dp_EACF_4.lammpstrj')
+            os.remove(in_file_location + '2dp_EACF_5.lammpstrj')
     except Exception as e:
         try:
-            f.writelines('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n')
-            f.writelines('sorry, it seems that something get wrong, and the wrong file is:\n')
+            f.writelines(
+                '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n'
+            )
+            f.writelines(
+                'sorry, it seems that something get wrong, and the wrong file is:\n'
+            )
             f.writelines(in_file_location)
             f.writelines('\nhere is the deatials:\n')
             f.writelines(e)
-        except Exception :
+        except Exception:
             pass
 f.writelines('All done, and this MD simulation finished at ')
-f.writelines( time.strftime("%Y-%m-%d %H:%M:%S \n", time.localtime()) )
+f.writelines(time.strftime("%Y-%m-%d %H:%M:%S \n", time.localtime()))
 f.writelines('=========================================================\n')
 f.close()
---
+++ bash_for_lammps/test.py
@@ -0,0 +1,146 @@
+
+#x = np.linspace(0, 2 * np.pi, 50)
+#plt.plot(x, np.sin(x)) # 如果没有第一个参数 x，图形的 x 坐标默认为数组的索引
+#plt.show()
+
+#print ("你好，世界")
+#box_block = 10
+#color     = 5
+
+#Simulation_box =[[[1 for i in range(box_block)] for i in range(box_block)] for i in range(color)]
+
+#print (Simulation_box)
+#plt.fill(x, y1, 'b', x, y2, 'r', alpha=0.3)
+
+#name = input ('input your name')   
+#print('hello',name)
+
+#a = 100
+#if a >= 0: 
+#    print(a)
+#else:
+#    print(-a)
+
+#print(ord('A'))
+#x = np.linspace(-np.py,np.pi,256,endpoint=ture)
+#C = np.cos(x)
+#S = np.sin(x)
+#while 1:
+
+# -*- coding: utf-8 -*-   
+  
+""" import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots()
+xdata, ydata = [], []
+ln, = ax.plot([], [], 'r-', animated=False)
+
+def init():
+    ax.set_xlim(0, 2*np.pi)
+    ax.set_ylim(-1, 1)
+    return ln,
+
+def update(frame):
+    xdata.append(frame)
+    ydata.append(np.sin(frame))
+    ln.set_data(xdata, ydata)
+    return ln,
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+                    init_func=init, blit=True)
+plt.show() """
+
+'''import numpy as np  
+import matplotlib.pyplot as plt  
+# 动画需要导入该模块  
+from matplotlib import animation  
+# 定义动画的速度，通过改变这个变量的值改变动画速度  
+speed = 0.3  
+  
+fig, ax = plt.subplots()  
+x = np.arange(0, 2*np.pi, 0.01)  
+line, = ax.plot(x, np.sin(x))  
+  
+# 每次执行时的函数，  
+def animate(i):  
+    line.set_ydata(np.sin(x + i * speed))  
+    return line,  
+  
+# 动画初始的方法  
+def init():  
+    line.set_ydata(np.sin(x))  
+    return line,  
+  
+# fig : 执行动画的图像  
+# func : 动画的执行函数  
+# frames : 表示多少次动画为一个循环  
+# init_func : 动画的初始位置  
+# interval : 动画执行的间隔  不能为小数,小数动画就不执行了,不知道是不是我的姿势不对  
+# blit : Mac设置为False,设置为True会报错,根据错误提示如下，可以使用'TKAgg'代替  
+# matplotlib.animation.BackendError: The current backend is 
+# 'MacOSX'and may go into an infinite loop with blit turned on.  
+# Either turn off blit or use an alternate backend, 
+# for example, like 'TKAgg', using the following prepended to your source code:  
+ani = animation.FuncAnimation(fig=fig,func=animate,frames=int(2*np.pi/speed),init_func=init,interval=1,blit=False)  
+# 查看帮助文档  
+#help(ani.save)  
+# 可以将动画以mp4格式保存下来，但首先要保证你已经安装了ffmpeg 或者mencoder  
+# ani.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])  
+  
+plt.show() '''
+'''import numpy
+a = [[1,2,3],[4,5,6]]
+b = []
+b = numpy.append(b,a)
+print(b)'''
+
+'''
+import os
+import sys
+import numpy
+import copy
+import codecs
+import shutil
+
+in_file_location = 'C:\\Users\\li199\\Desktop\\Python_test\\Data\\Kappa2'+ '%' +'Gamma20_%Beta0.0%N4096_%Compare0%'
+variable_number = 1
+temp = 'test' + '\n'
+
+in_file = codecs.open(in_file_location+'\\in.2dplasma', 'r+')#,encoding='ISO-8859-1')
+lines = []
+for line in in_file:
+    lines.append(line)
+in_file.close()
+
+
+variable_number = lines.find('atom_style      atomic')
+lines.insert(variable_number,temp)
+lines.insert(variable_number,temp)
+lines.insert(variable_number,temp)
+lines.insert(variable_number,temp)
+s = ''.join(lines)
+
+#print(lines)
+
+in_file = open(in_file_location+'\\py.2dplasma', 'w',encoding='ISO-8859-1')
+in_file.write(s)
+in_file.close()
+'''
+
+#a = 'Gamma20beta0.2'
+#print(a.find('beta'))
+#a = 'Kappa2\\Gamma300\\Beta0.0\\N16384\\Compare0\\'
+#print('variable        Gamma           equal')
+'''a = ['1','2']
+for i in a:
+    try:
+        print(i)
+    except:
+        pass
+import sys
+f = open(sys.path[0] + 'log.txt', 'a')'''
+
+import os
+print()
