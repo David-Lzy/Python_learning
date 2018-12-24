099c2af7228bb3a958591679253cee4cad5b95f6
David_Lzy <li19931020li@gmail.com>
2018-12-24 16:49:59 +0800

    tensor_flow_learning

--- Python_test/meachine_learing/test3.py
+++ Python_test/meachine_learing/test3.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 a = tf.constant([1.0,2.0],name='a')
 b = tf.constant([2.0,2.0,],name='b')
+g = tf.Graph()
+with g.device('/gpu:0'):
 result = a+b
 sess = tf.Session()
 sess.run(result)
